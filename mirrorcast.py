a256deb80f0c8525cc865bd2b40b68d5f244db2b
robert.grupp <robert.grupp@afd40d5d-5021-b3a1-b3f9-c7dcecc69323>
2010-10-01 05:11:33 +0000

    initial commit * very basic functionality * only configurable option is the port of the server * does not support Icecast UDP metadata

---
+++ mirrorcast.py
@@ -0,0 +1,79 @@
+
+import BaseHTTPServer
+import SocketServer
+import urllib2
+import urllib
+import sys
+import time
+
+kCHUNK_SIZE = 64 * 1024
+
+class ThreadedHttpServer(SocketServer.ThreadingMixIn,
+                         BaseHTTPServer.HTTPServer):
+    pass
+
+class RadioForwarderHttpHandler(
+                      BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_POST(self):
+        self.send_error(404)
+
+    def do_GET(self):
+        # requests to mirror take the form /mirror/<radio URL>
+        # eg. /mirror/http://75.102.43.202/live
+        path_str = self.path[1:]
+        kMIRROR_ACTION = 'mirror'
+        kMIRROR_ACTION_W_SLASH = '%s/' % kMIRROR_ACTION
+        path_entries = path_str.split('/')
+        if (path_str and (path_entries[0].lower() == kMIRROR_ACTION)
+            and (len(path_str) > len(kMIRROR_ACTION_W_SLASH))):
+            # make sure it has a / followed by an address
+            if path_str.find(kMIRROR_ACTION_W_SLASH) == 0:
+                srv_url = urllib.unquote(
+                    path_str[len(kMIRROR_ACTION_W_SLASH):])
+                # retreive the client's headers
+                cli_hdrs = {}
+                # for k,v in self.headers does not work, yields
+                # the following error:
+                # ValueError: too many values to unpack
+                # solution is to just to do for k in self.headers
+                # and then lookup the value.
+                for cli_hdr_key in self.headers:
+                    # currently no support for 'x-audiocast-udpport'
+                    cli_hdr_val = self.headers[cli_hdr_key]
+                    if cli_hdr_val.lower() != 'x-audiocast-udpport':
+                        cli_hdrs[cli_hdr_key] = cli_hdr_val
+                # make the connection to the real radio server
+                u = urllib2.urlopen(urllib2.Request(srv_url,
+                                    headers=cli_hdrs))
+                # send back header
+                self.send_response(200)
+                u_info = u.info()
+                for srv_hdr_key in u_info:
+                    self.send_header(srv_hdr_key,u_info[srv_hdr_key])
+                self.end_headers()
+                # now start writing the audio stream
+                while True:
+                    chunk = u.read(kCHUNK_SIZE)
+                    if chunk:
+                        try:
+                            self.wfile.write(chunk)
+                        except:
+                            # exception will be thrown on client exit
+                            break
+                    else:
+                        time.sleep(0.05) # 50 ms
+            else:
+                self.send_error(404)
+        else:
+            self.send_error(404)
+
+
+def main_fn():
+    try:
+        ThreadedHttpServer(('',int(sys.argv[1])),
+                       RadioForwarderHttpHandler).serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+   main_fn()
